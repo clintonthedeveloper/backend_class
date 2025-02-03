a7d8fedc1c215cecfea9657ae34deb4921f57cf3
Malcom Otieno Adrian <malcolmotieno16@gmail.com>
2025-02-03 16:56:52 +0300

    Update Building a simple login sign-in system.py

--- Bulding a simple login singin system.py
+++ Bulding a simple login singin system.py
@@ -1,15 +1,28 @@
-print('Create an Account now')
-username = input('Enter user name: ')
-password = input('Enter password:')
+question = input('Do you have an account? (Y or N): ').lower()
 
+if question == 'n':
+    print('Welcome to the Signup page, Please fill in these details')
+    email = input('Enter Your Email: ')
+    username = input('Enter username: ')
+    password = input('Enter password: ')
+    password2 = input('Re-Enter Password: ')
 
-print('Your account has been created succefully')
-print('Login now!')
-
-username2 = input('Enter username: ')
-password2 = input('Enter password: ')
-
-if username == username2 and password == password2:
-    print('Login successfully')
+    if password == password2:
+        print('Your account has been created successfully')
+        print('Redirecting to Login Page, Please Wait...')
+    else:
+        print('Passwords don\'t match, Kindly check for inconsistencies!')
+        print('Exiting Program!')
 else:
-    print('Invalied credentials')    
+    if question == 'y':
+        username2 = input('Enter username: ')
+        password2 = input('Enter password: ')
+
+        if username2 == username and password2 == password:
+            print('Login successfully')
+        else:
+            print('Invalid credentials')
+    else:
+        print('Invalid Input, Try Again!!')
+
+
